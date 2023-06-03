d9c2ab3ebb6b81604cc909ed7e502a77c55b74d2
Minimage <seancampbell914.com>
2023-06-03 01:02:57 -0400

    trying something new

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   useEffect(() => {
     if (scanResults != null) {
       Axios.post("http://dryexpress.herokuapp.com/createOrder", {
-        scanResults: userId,
+        scanResults: scanResults,
       });
     }
   }, scanResults);
