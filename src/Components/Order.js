bef97e3c279304b5e24b1f920c0a6288b7b08473
Minimage <seancampbell914.com>
2023-06-08 13:29:40 -0400

    added a get requst to grab orders

--- src/Components/Order.js
+++ src/Components/Order.js
@@ -1,11 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Axios from "axios";
 
 export const Order = ({ user }) => {
-  const getOrders = () => {
-    console.log(user);
-  };
+  const [orders, setOrders] = useState([]);
 
-  console.log(user);
-  return <div>Order</div>;
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const response = await Axios.get(`/orders?userId=${user._id}`);
+        setOrders(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchOrders();
+  }, [user]);
+
+  return (
+    <div>
+      <h2>Orders</h2>
+      {orders.map((order) => (
+        <div key={order._id}>
+          <p>Order ID: {order._id}</p>
+          <p>Date: {order.dateTime}</p>
+          {/* Display other order details */}
+        </div>
+      ))}
+    </div>
+  );
 };
