b591b9e9d0ca79ce82b9dc9bd98b349cc7b9ae51
Minimage <seancampbell914.com>
2023-06-08 15:36:41 -0400

    this should fix the error

--- src/Components/Order.js
+++ src/Components/Order.js
@@ -7,30 +7,40 @@ export const Order = ({ user }) => {
   useEffect(() => {
     const fetchOrders = async () => {
       try {
+        const token = localStorage.getItem("token"); // Retrieve the token from localStorage or wherever you store it
         const response = await Axios.get(
-          `https://dryexpress.herokuapp.com/orders?userId=${user._id}`
+          `https://dryexpress.herokuapp.com/orders?userId=${user._id}`,
+          {
+            headers: {
+              "x-access-token": token, // Include the token in the request headers
+            },
+          }
         );
-        setOrders(response.data);
+        const data = response.data;
+        setOrders(data);
       } catch (error) {
-        console.log(error);
+        console.error(error);
       }
     };
 
-    if (user) {
-      fetchOrders();
-    }
+    fetchOrders();
   }, [user]);
 
   return (
     <div>
-      <h2>Orders</h2>
-      {orders.map((order) => (
-        <div key={order._id}>
-          <p>Order ID: {order._id}</p>
-          <p>Date: {order.dateTime}</p>
-          {/* Display other order details */}
-        </div>
-      ))}
+      <h1>Orders</h1>
+      {orders.length > 0 ? (
+        orders.map((order) => (
+          <div key={order._id}>
+            <p>First Name: {order.firstName}</p>
+            <p>Last Name: {order.lastName}</p>
+            <p>Date and Time: {order.formattedDateTime}</p>
+          </div>
+        ))
+      ) : (
+        <p>No orders found.</p>
+      )}
+      {console.log(orders)}
     </div>
   );
 };
