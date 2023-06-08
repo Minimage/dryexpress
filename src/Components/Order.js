594d0a5775f899d47a762036a83a93e4a429056f
Minimage <seancampbell914.com>
2023-06-08 12:46:50 -0400

    Created an order and admin page.
    Cant access pages without logging in

---
+++ src/Components/Order.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+import Axios from "axios";
+
+export const Order = ({ user }) => {
+  const getOrders = () => {
+    console.log(user);
+  };
+
+  console.log(user);
+  return <div>Order</div>;
+};
