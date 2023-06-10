5f6088f96e5b4ab1e0de340af8dbc45ed2bb49ee
Minimage <seancampbell914.com>
2023-06-10 17:06:11 -0400

    pushing code

--- src/Components/RegEmployee.js
+++ src/Components/RegEmployee.js
@@ -1,14 +1,13 @@
 import React, { useEffect } from "react";
 import Axios from "axios";
 
-useEffect(() => {
-  Axios.get("https://dryexpress.herokuapp.com/getApartments")
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {});
-}, []);
-
 export const RegEmployee = () => {
+  useEffect(() => {
+    Axios.get("https://dryexpress.herokuapp.com/getApartments")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {});
+  }, []);
   return <div>RegEmployee</div>;
 };
