97cbeb06204d82132cbb7eff9791585ee5b09f64
Minimage <seancampbell914.com>
2023-06-11 17:41:17 -0400

    created a page to show the aparment attached to the user

--- src/Components/Apartments.js
+++ src/Components/Apartments.js
@@ -2,34 +2,30 @@ import React, { useState, useEffect } from "react";
 import Axios from "axios";
 
 export const Apartments = ({ employee }) => {
-  console.log(employee.apartmentID);
+  const [aptID, setAptID] = useState(employee.apartmentID);
+  const [data, setData] = useState();
+
+  console.log(employee);
   useEffect(() => {
-    Axios.get(
-      `https://dryexpress.herokuapp.com/apartments/${employee.ApartmentID}`
-    )
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+    if (employee.apartmentID) {
+      Axios.get(
+        `https://dryexpress.herokuapp.com/apartments/${employee.apartmentID}`
+      )
+        .then((response) => {
+          console.log(response);
+          setData(response.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [employee.ApartmentID]);
 
-  //   useEffect(() => {
-  //     const fetchOrders = async () => {
-  //       try {
-  //         const response = await Axios.get(
-  //           `https://dryexpress.herokuapp.com/apartments/${employee.ApartmentID}`
-  //         );
-  //         const data = response.data;
-  //         console.log(data);
-  //       } catch (error) {
-  //         console.error(error);
-  //       }
-  //     };
-
-  //     fetchOrders();
-  //   }, []);
-
-  return <div>Apartments</div>;
+  return (
+    <div>
+      <h1>{data?.apartment?.apparmentName}</h1>
+      <br />
+      <span>{data?.apartment?.address}</span>
+    </div>
+  );
 };
