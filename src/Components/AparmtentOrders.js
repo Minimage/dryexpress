714ce86f9e7701102cae73a2a486b06f9351193c
Minimage <seancampbell914.com>
2023-07-01 20:00:42 -0400

    forgot to add full url and thats where the error was

--- src/Components/AparmtentOrders.js
+++ src/Components/AparmtentOrders.js
@@ -4,13 +4,14 @@ import Axios from "axios";
 //  This page should display all orders that the apartment has
 
 export const AparmtentOrders = () => {
+  const [orders, setOrders] = useState([]);
   useEffect(() => {
     console.log(localStorage.getItem("ApartmentID"));
-    Axios.post("/getOrderByApartmentID", {
+    Axios.post("http://dryexpress.herokuapp.com/getOrderByApartmentID", {
       apartmentID: localStorage.getItem("ApartmentID"),
     })
       .then((response) => {
-        console.log(response);
+        console.log(response.data);
       })
       .catch((err) => {
         console.log(err);
