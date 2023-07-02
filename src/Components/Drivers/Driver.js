3fe6c3e157ab6235fba80e2b71a173182e54c7a7
Minimage <seancampbell914.com>
2023-07-02 03:27:55 -0400

    added components for the drivers to recieve orders from facilites and bring to cleaners and vise verca

---
+++ src/Components/Drivers/Driver.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Driver = () => {
+  return <div>Driver</div>;
+};
---
+++ src/Components/Drivers/DriverGoing.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Driver_Going = () => {
+  return <div>Driver_Going</div>;
+};
